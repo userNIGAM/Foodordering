79409664df51106bcaaff0a1c5031f9079829a59
userNIGAM <nigam333nigam@gmail.com>
2025-09-12 15:55:00 +0545

    modified menu

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,13 @@
 // src/services/api.js
 import axios from "axios";
 
+// Define baseURL once
+const baseURL =
+  import.meta.env.VITE_API_BASE ||
+  (import.meta.env.MODE === "development"
+    ? "http://localhost:5000"
+    : "https://foodordering-i801.onrender.com");
+
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_BASE ||
